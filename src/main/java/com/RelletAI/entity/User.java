e273f4926b9299effa226a4ce0631d08440c9514
Harsh Pathak <83153262+iharshpathak@users.noreply.github.com>
2026-05-31 07:38:27 +0000

    added db and configs

---
+++ src/main/java/com/RelletAI/entity/User.java
@@ -0,0 +1,26 @@
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private String imageUrl;
+
+    private Boolean subscription = false;
+}
